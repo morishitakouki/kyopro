d8716e70b063f9b81590004641f8e356263dbec6
uyudane <sy.yun@icloud.com>
2022-12-19 17:39:28 +0900

    Add: DP1と2を追加

--- DP/DPまとめA.rb
+++ DP/DPまとめA.rb
@@ -1,25 +1,40 @@
 # https://atcoder.jp/contests/dp/tasks/dp_a
 
-N = gets.chomp.to_i
+# 解答案　
+n = gets.to_i
+h = gets.chomp.split.map(&:to_i)
 
-H = gets.chomp.split.map(&:to_i)
+d = Array.new(n, 10 ** 4 + 1)
+d[0] = 0
+d[1] = (h[0] - h[1]).abs
 
-i = 0
-
-ans = []
-
-ans[0] = 0
-
-ans[1] = (H[0]-H[1]).abs
-
-if N >= 3
-  ans[2] = [ans[0] + (H[0] - H[2]).abs, ans[1] + (H[1] - H[2]).abs].min
+2.upto(n-1) do |i|
+  d[i] = [d[i-1] + (h[i-1] - h[i]).abs, d[i-2] + (h[i-2] - h[i]).abs].min
 end
+puts d[n-1]
 
-if N >= 4
-  (3..N-1).each do |n|
-    ans[n] = [ans[n-2] + (H[n-2] - H[n]).abs, ans[n-1] + (H[n-1] - H[n]).abs].min
-  end
-end
+# 自身の回答
 
-puts ans[N-1]
+# N = gets.chomp.to_i
+
+# H = gets.chomp.split.map(&:to_i)
+
+# i = 0
+
+# ans = []
+
+# ans[0] = 0
+
+# ans[1] = (H[0]-H[1]).abs
+
+# if N >= 3
+#   ans[2] = [ans[0] + (H[0] - H[2]).abs, ans[1] + (H[1] - H[2]).abs].min
+# end
+
+# if N >= 4
+#   (3..N-1).each do |n|
+#     ans[n] = [ans[n-2] + (H[n-2] - H[n]).abs, ans[n-1] + (H[n-1] - H[n]).abs].min
+#   end
+# end
+
+# puts ans[N-1]
--- DP/DPまとめB.rb
+++ DP/DPまとめB.rb
@@ -1,28 +1,65 @@
 # https://atcoder.jp/contests/dp/tasks/dp_b
 
-N,K = gets.chomp.split.map(&:to_i)
+# 回答例
+# nで+1,+2,+3..、n+1で+1,+2,+3とどんどん上書きしていくイメージ。そのため、計算量はO(NK)回になる。
+N,K = gets.chomp.split(" ").map(&:to_i)
+H = gets.chomp.split(" ").map(&:to_i)
 
-H = gets.chomp.split.map(&:to_i)
+# 先に配列を準備してあげる必要がある。
+dp = Array.new(N, Float::INFINITY)
+dp[0] = 0
 
-ans = []
-
-ans[0] = 0
-
-# 初期値のKまでの値を設定する。
-if N > K
-  p "ねこ"
-  # N = 1の場合のans[0]は設定したため、
-  p K-1
-  0.upto((K-1)) do |k|
-    # 直接0から設定するコストを初期値として設定する
-    ans[k+1] = (H[0] - H[k+1]).abs
-    # ans[l]から飛んだ際のコストを出していく
-    # K以下のため、k+1回繰り返す
-    (k+1).upto do |l|
-      tmp = ans[l] + (H[l] - H[k+1]).abs
-      ans[k+1] = tmp if ans[k+1] > tmp
-    end
+(N - 1).times do |i|
+  (1..K).each do |v|
+    next if i + v >= N
+    dp[i + v] = [dp[i] + (H[i + v] - H[i]).abs, dp[i + v]].min
   end
 end
 
-p ans
+puts dp.last
+
+# # 自身の回答
+
+# N,K = gets.chomp.split.map(&:to_i)
+
+# H = gets.chomp.split.map(&:to_i)
+
+# ans = []
+
+# ans[0] = 0
+
+# # 初期値のKまでの値を設定する。
+# if N > K
+#   # N = 1の場合のans[0]は設定したため、K-1回になる
+#   1.upto((K)) do |k|
+#     # 直接0から設定するコストを初期値として設定する
+#     ans[k] = (H[0] - H[k]).abs
+#     # 0からans[l]までを最小値で飛び、ans[l]からans[k+1]まで飛んだ際のコストを出していく
+#     # K以下のため、k回繰り返す
+#     (1).upto(k) do |l|
+#       tmp = ans[l] + (H[l] - H[k]).abs
+#       ans[k] = tmp if ans[k] > tmp
+#     end
+#   end
+
+#   (K+1).upto(N-1) do |m|
+#     ans[m] = ans[m-K] + (H[m-K] - H[m]).abs
+
+#     1.upto(K) do |n|
+#       tmp = ans[m-K+n] + (H[m-K+n] - H[m]).abs
+#       ans[m] = tmp if ans[m] > tmp
+#     end
+#   end
+# end
+
+# if N <= K
+#   1.upto((N-1)) do |k|
+#     ans[k] = (H[0] - H[k]).abs
+#     (1).upto(k) do |l|
+#       tmp = ans[l] + (H[l] - H[k]).abs
+#       ans[k] = tmp if ans[k] > tmp
+#     end
+#   end
+# end
+
+# puts ans.last
